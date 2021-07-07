26b191ffd202f16a54e857fe053c4d82619ca726
YugAadiwasi <shivoopavagadhi36@gmail.com>
2021-07-07 16:54:28 +0530

    Initial commit

---
+++ assets/js/experience.js
@@ -0,0 +1,83 @@
+AOS.init();
+
+//  Work experience cards
+
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "Software Development Intern",
+    cardImage: "assets/images/experience-page/flipkart.jpg",
+    place: "Flipkart",
+    time: "(May, 2020 - present)",
+    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+  },
+  {
+    title: "Student Developer",
+    cardImage: "assets/images/experience-page/gsoc.png",
+    place: "Google Summer Of Code",
+    time: "(Mar - Aug, 2019)",
+    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+  },
+];
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+const hackathonsection = document.querySelector(".hackathon-section");
+const mentor = [
+ {
+    title: "NHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/nhacks.png",
+    desp: "NHacks VI was a hackathon that aimed to encourage students everywhere to develop skills in computer science by creating tangible products that they're proud of. It initially started as Michigan's largest high school hackathon.",
+  }
+  
+
+];
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, subtitle, desp}) =>
+      (output += `  
+      <div class="blog-slider__item swiper-slide">
+        <div class="blog-slider__img">
+            <img src="${image}" alt="">
+        </div>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">${title}</div>
+          <span class="blog-slider__code">${subtitle}</span>
+          <div class="blog-slider__text">${desp}</div>
+          <a href="#" class="blog-slider__button">Read More</a>   
+        </div>
+      </div>
+      `)
+  );
+  hackathonsection.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
---
+++ assets/js/research.js
@@ -0,0 +1,65 @@
+/*Research Details Table*/
+
+const researchTable = document.querySelector(".main");
+
+const research = [
+ {
+        title : "Forward and Backward Information Retention for Accurate Binary Neural Networks",
+        authors : "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan",
+        conferences : "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        researchYr : 2020,
+        citebox : "popup8",
+        image : "assets/images/research-page/neuralNetworks.jpg",
+        citation: {
+            vancouver: "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan. Forward and Backward Information Retention for Accurate Binary Neural Networks. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020."
+        },
+        abstract: "This is currently left empty and this can be considered as a dummy data 8",
+        absbox: "absPopup8"
+    }
+];
+AOS.init();   
+const fillData = () => {
+    let output = "";
+    research.forEach(
+        ({image, title, authors, conferences, researchYr, citebox, citation, absbox, abstract}) =>
+        (output +=`
+            <tr data-aos="zoom-in-left"> 
+                <td class="imgCol"><img src="${image}" class="rImg"></td>
+                <td class = "researchTitleName">
+                    <div>
+                        <span class="imgResponsive">
+                            <img src="${image}" class="imgRes">
+                        </span>
+                    </div>
+                    <a href="#0" class="paperTitle"> ${title} </a> 
+                    <div> ${authors} </div> <div class="rConferences"> ${conferences} 
+                        <div class="researchY">${researchYr}</div>
+                    </div>
+        
+                    <!--CITE BUTTON-->
+                    <div class="d-flex" style="margin-right:5%;">
+                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
+                            ABSTRACT
+                        </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
+                            CITE
+                        </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </div>
+                    <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
+                        <div class="card-body">
+                            ${abstract}    
+                        </div>
+                    </div>
+                    <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
+                        <div class="card-body">
+                            ${citation.vancouver}    
+                        </div>
+                    </div>
+                </td>
+            </tr>`)
+        );
+    researchTable.innerHTML = output;
+
+};
+document.addEventListener("DOMContentLoaded", fillData);
